59c58462b781482d376e2451c9faa2322aa6477b
Sebastián Demaría <53883705+sebdemaria@users.noreply.github.com>
2023-03-23 23:16:14 -0300

    HOTFIX(#002): import for deploy

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,4 +1,4 @@
 export { default as MenuBtnOpen } from "./header/menu-btn-open.svg";
 export { default as MenuBtnClose } from "./header/menu-btn-close.svg";
 export { default as ProfileImg } from "./header/profile-image.webp";
-export { default as Notification } from "./header/Notification.svg";
+export { default as Notification } from "./header/notification.svg";
